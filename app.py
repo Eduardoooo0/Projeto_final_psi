5c0a5e25595581a70edaa095b2f1509415e489ac
Eduardoooo0 <el957733@gmail.com>
2025-02-22 16:53:47 -0300

    cofigurando flask-mail

--- app.py
+++ app.py
@@ -7,10 +7,21 @@ from controllers.patients import patients_bp
 from flask_login import LoginManager
 import os
 from werkzeug.security import generate_password_hash
+from flask_mail import Mail, Message
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'xoxorroxupetao'
+
+app.config['MAIL_SERVER'] = os.getenv('SERVER')
+app.config['MAIL_PORT'] = int(os.getenv('PORT'))
+app.config['MAIL_USERNAME'] = os.getenv('EMAIL')
+app.config['MAIL_PASSWORD'] = os.getenv('PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('USERNAME')
+app.config['MAIL_USE_TLS'] = True
+
+mail = Mail(app)
 
 db.init_app(app)
 
@@ -31,11 +42,14 @@ app.register_blueprint(patients_bp, url_prefix='/patients')
 
 @app.route('/')
 def index():
+    msg = Message('EAE MINHA PATROA',
+                sender=os.getenv('EMAIL'),  # Seu email
+                recipients=['e.giscely@escolar.ifrn.edu.br'])  # Email do destinatário
+    msg.body = 'Solteira?'
+    mail.send(msg)
     user = User.query.filter_by(tipo='admin').first()
-    if user:
-        print('oi')
-    else:
-        user = User(nome=os.getenv('NOME'), email=os.getenv('EMAIL'), senha=generate_password_hash(os.getenv('SENHA')), tipo=os.getenv('TIPO'))
+    if user is None:
+        user = User(nome=os.getenv('NOME_ADMIN'), email=os.getenv('EMAIL_ADMIN'), senha=generate_password_hash(os.getenv('SENHA_ADMIN')), tipo=os.getenv('TIPO_ADMIN'))
         db.session.add(user)
         db.session.commit()
     return render_template('index.html')
